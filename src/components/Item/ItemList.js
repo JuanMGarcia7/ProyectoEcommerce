e85d2622e682255d97e555698a036dfd2ea3c63d
Juan Manuel Garcia <juanmagarcia.7@hotmail.com>
2021-10-11 16:41:17 -0300

    Agregando loader

--- src/components/Item/ItemList.js
+++ src/components/Item/ItemList.js
@@ -1,30 +1,36 @@
 
 import React, { useState, useEffect } from 'react';
 import Item from './Item';
+import Spinner from "../Spinner/Spinner"
+
 
 
 const ItemList = () => {
     
     const [items , setItems] = useState([])
-   
+    const [isLoading , SetIsLoading] = useState(true)
     
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then((response) => response.json())
         .then((data) =>setItems(data));
+        setTimeout(()=>{
+            SetIsLoading(false);
+        },2000)       
     }, []);
 
    
     
     return (
-        <div>
-            <h1>ITEM</h1>
+        <div>       
+                    
+            <h1>ITEMS</h1>         
             {items.map ((item) =>{
                 
                 return(
                 
                     <div key={item.id}>
-                        <Item data={item} />
+                        {isLoading? <Spinner /> :   <Item data={item} />  }    
                     </div>
                     )
                 
