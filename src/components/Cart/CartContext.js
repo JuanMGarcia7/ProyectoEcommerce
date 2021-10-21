ee23cddcf5ea2a44d1de4a2d83d4de6f98c1ba69
Juan Manuel Garcia <juanmagarcia.7@hotmail.com>
2021-10-21 15:03:39 -0300

    Fixed count en itemDetail

---
+++ src/components/Cart/CartContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [carrito, setCarrito] = useState([]);
+
+  return (
+    <CartContext.Provider value={(carrito, setCarrito)}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+/* Pasar las props aca */
+
+export default CartContext;
