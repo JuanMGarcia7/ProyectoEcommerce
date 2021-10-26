6be13c86497db236de91e0ad14cd659795273043
Juan Manuel Garcia <juanmagarcia.7@hotmail.com>
2021-10-25 22:58:19 -0300

    Configurando cart (tp context)

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,17 +2,23 @@ import React, { useContext } from "react";
 import CartContext from "../Context/CartContext";
 import { Table } from "semantic-ui-react";
 import "./Cart.css";
+import { Link } from "react-router-dom";
 const Cart = () => {
   const [cart, setCart] = useContext(CartContext);
+
   console.log(cart);
 
   const elminarItem = (item) => {
     cart.splice(
-      cart.findIndex((i) => i.item === item),
+      cart.findIndex((i) => i.item === item.id),
       1
     );
 
     setCart([...cart]);
+  };
+
+  const clearCart = () => {
+    setCart([]);
   };
 
   return (
@@ -20,7 +26,9 @@ const Cart = () => {
       <div>
         {cart.length <= 0 ? (
           <div className="carroVacio">Su carro esta vacio </div>
-        ) : null}
+        ) : (
+          <div className="carroVacio">Este es su carro de compras! </div>
+        )}
       </div>
       {cart.map((item) => (
         <div key={cart.id} className="tableCart">
@@ -29,10 +37,23 @@ const Cart = () => {
             <td className="tdCart">Producto: {item.itemTitle}</td>
             <td className="tdCart">Cantidad: {item.cantidadDeItems}</td>
             <td className="tdCart"> Precio: u$s{item.price}</td>
-            <button onClick={elminarItem}>Eliminar del carro</button>
+            <button onClick={elminarItem} className="tdCart btnEliminar">
+              Eliminar item
+            </button>
           </tr>
         </div>
       ))}
+      <div>
+        {cart.length > 0 ? (
+          <button onClick={clearCart} className="btnEliminar">
+            Vaciar carro!
+          </button>
+        ) : (
+          <Link to="/">
+            <div>Volver al listado</div>
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
